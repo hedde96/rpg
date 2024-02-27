4701886a08fe2a134de06bf79ea372fc1bfd01d2
hedde <hedde3131@gmail.com>
2024-02-27 10:16:55 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        int passwordLength = 25;
+        string generatedPassword = GenerateRandomPassword(passwordLength);
+
+        Console.WriteLine("Generated Password: " + generatedPassword);
+    }
+
+    static string GenerateRandomPassword(int length)
+    {
+        const string characterSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
+        
+        StringBuilder password = new StringBuilder();
+
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++)
+        {         
+            password.Append(characterSet[random.Next(characterSet.Length)]);
+        }
+
+        return password.ToString();
+    }
+}
